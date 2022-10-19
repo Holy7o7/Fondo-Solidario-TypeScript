02b620123f3c7707dcaedea4066016d83513069a
Francisco Fuentes <43901671+francisss-s@users.noreply.github.com>
2022-10-18 22:16:38 -0300

    Mejora visual

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import Linechart from './Charts/Linechart';
 function App() {
   return (
     <div>
-      <h1>                     Ejemplos Graficos </h1>
+      <h1 className='headers'>Ejemplos Graficos </h1>
       <div className='parent'>
         <div className='graph'><Barchart/></div>
+        <p><input type="reset" className='button' value={"Boton de ejemplo"}/></p>
+        
       </div>
       <div className='parent'>
         <div className='graph'><Linechart/></div>
+        <input type="button" className='button' value={"Boton de ejemplo"} />
       </div>
       <div className='parent'>
-        <div className='graph'> <h4>Grafico Velocimetro de alcance de metas</h4><Gauge/></div>
+        <div className='graph'> <h4 className='graphTytle'>Grafico Velocimetro de alcance de metas</h4><Gauge/></div>
+        <input type="button" className='button' value={"Boton de ejemplo"} />
       </div>      
     </div>
   );
